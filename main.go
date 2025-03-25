670986623f112695a2dea646d9d777a4ebf449c9
hash-walker <hamzafaraz1821@gmail.com>
2025-03-25 23:55:16 +0500

    building the bones of REPL like the input and output

--- main.go
+++ main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-
-func main(){
-	fmt.Println("Hello, World!")
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		text := scanner.Text()
+		words := cleanInput(text)
+		fmt.Printf("Your command was: %s\n", words[0])
+		fmt.Print("Pokedex > ")
+	}
 }
