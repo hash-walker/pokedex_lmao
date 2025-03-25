01a1033d7d112ecd7f8d1500132dbd549b30e9f6
hash-walker <hamzafaraz1821@gmail.com>
2025-03-26 00:00:53 +0500

    modified the repl and main file, added the basic functionality to the repl file and only using the main file for execution of repl

--- main.go
+++ main.go
@@ -1,18 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Print("Pokedex > ")
-	for scanner.Scan() {
-		text := scanner.Text()
-		words := cleanInput(text)
-		fmt.Printf("Your command was: %s\n", words[0])
-		fmt.Print("Pokedex > ")
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,30 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		text := scanner.Text()
+
+		words := cleanInput(text)
+
+		if len(words) == 0 {
+			continue
+		}
+
+		firstCommand := words[0]
+
+		fmt.Printf("Your command was: %s\n", firstCommand)
+	}
+}
 
 func cleanInput(text string) []string {
 	output := strings.ToLower(text)
